5c66582b25851960bb13726a4236cd501716e82e
Mikhail Medvedev <mmedvedev@tenable.com>
2019-07-30 17:01:21 +0100

    Bubble sort

---
+++ bubblesort.go
@@ -0,0 +1,14 @@
+package main
+
+// BubbleSort bubble sort implementation
+func BubbleSort(arr []int) []int {
+	l := len(arr)
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+			}
+		}
+	}
+	return arr
+}
